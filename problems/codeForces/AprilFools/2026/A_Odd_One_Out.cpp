aaf76398587a02e25d5920dbc56251185cc1c935
Arnab Sarkar <68094959+28arnab@users.noreply.github.com>
2026-04-13 01:29:27 +0530

    April fools contest 2026
    Signed-off-by: Arnab Sarkar <68094959+28arnab@users.noreply.github.com>

---
+++ problems/codeForces/AprilFools/2026/A_Odd_One_Out.cpp
@@ -0,0 +1,44 @@
+/* Author: Arnab Sarkar */
+#ifndef ONLINE_JUDGE
+#include "cp_header.hpp"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+using ll = long long;
+using ld = long double;
+using st = string;
+
+#define F first
+#define S second
+#define B begin
+#define E end
+
+#define REP(i, a, b) for (ll i = (a); i <= (b); i++)
+#define PER(i, a, b) for (ll i = (b); i >= (a); i--)
+
+using vll = vector<ll>;
+using vld = vector<ld>;
+using vs = vector<st>;
+
+using pll = pair<ll, ll>;
+using pld = pair<ld, ld>;
+using ps = pair<st, st>;
+
+void solve() { cout << "C2"; }
+
+int32_t main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  //   cin >> t;
+  while (t--)
+  {
+    solve();
+    // cout << endl;
+  }
+  return 0;
+}
